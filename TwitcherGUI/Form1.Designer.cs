cd214665e105f1241f80e6a5d95103c3d6070d60
kserio <konieczny93@gmail.com>
2014-12-09 23:33:57 +0100

    Initial commit

---
+++ TwitcherGUI/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TwitcherGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.channelsListView = new System.Windows.Forms.ListView();
+            this.channelName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gameName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.playButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // channelsListView
+            // 
+            this.channelsListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.channelsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.channelName,
+            this.gameName});
+            this.channelsListView.Location = new System.Drawing.Point(10, 22);
+            this.channelsListView.Name = "channelsListView";
+            this.channelsListView.Size = new System.Drawing.Size(329, 443);
+            this.channelsListView.TabIndex = 1;
+            this.channelsListView.UseCompatibleStateImageBehavior = false;
+            this.channelsListView.View = System.Windows.Forms.View.Tile;
+            this.channelsListView.SelectedIndexChanged += new System.EventHandler(this.channelsListView_SelectedIndexChanged);
+            // 
+            // channelName
+            // 
+            this.channelName.Text = "Streamer";
+            this.channelName.Width = 100;
+            // 
+            // gameName
+            // 
+            this.gameName.Text = "Game";
+            this.gameName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(155, 13);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(80, 27);
+            this.refreshButton.TabIndex = 0;
+            this.refreshButton.Text = "Refresh";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.playButton);
+            this.groupBox1.Controls.Add(this.refreshButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 471);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 46);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select game";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(241, 13);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(80, 27);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 529);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.channelsListView);
+            this.Name = "Form1";
+            this.Text = "TwitcherGUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView channelsListView;
+        private System.Windows.Forms.ColumnHeader channelName;
+        private System.Windows.Forms.ColumnHeader gameName;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+
+    }
+}
+
---
+++ TwitcherGUI/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace TwitcherGUI
+{
+    public partial class Form1 : Form
+    {
+
+        private RootObject getChannelList()
+        {
+            var streamsUri = new Uri("https://api.twitch.tv/kraken/streams");
+            RootObject ro;
+
+            using (var webClient = new WebClient())
+            {
+                var json = webClient.DownloadString(streamsUri);
+                // Now parse with JSON.Net
+                ro = JsonConvert.DeserializeObject<RootObject>(json);
+            }
+
+            return ro;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RootObject ro = getChannelList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Stream stream = ro.streams.ElementAt(i);
+                int streamViewersInt = Convert.ToInt32(stream.viewers);
+                string streamViewers = streamViewersInt.ToString("#,000");
+                string[] row = { stream.game, streamViewers };
+                channelsListView.Items.Add(stream.channel.name).SubItems.AddRange(row);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        // 100 000
+        // 
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void channelsListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
